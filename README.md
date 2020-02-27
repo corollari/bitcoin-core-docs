abba4bc9f93755f2f241b40448f92d5f695b9f4b
Albert <github@albert.sh>
2020-02-27 17:27:22 +0800

    Push initial version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bitcoin-core-docs
-Website that hosts bitcoin core docs
+> Website that hosts bitcoin core docs
+
+Hacky website that I built in order to view the docs (not including release info) in a more comfortable and searchable (on all.html) way.
+
+Essentially, it works by triggering a github actions job once a day which
+
+1. Pulls the bitcoin repo
+2. Converts the markdown documents in `/doc` (excluding release notes) into html using pandoc
+3. Publishes the resulting html into Github Pages
---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+md2html () {
+	pandoc -t html -f gfm --css "pandoc.css" -s $1 -o $2 -V header-includes:"<link rel='shortcut icon' href='favicon.png' />" --metadata pagetitle="$3" --fail-if-warnings
+}
+
+git clone https://github.com/bitcoin/bitcoin.git
+doc_filenames=$(ls bitcoin/doc/*.md | grep -v release-notes)
+sed -i s/\.md/\.html/g $doc_filenames
+mkdir -p docs
+cp pandoc.css docs
+echo "" > index.md
+
+cat $doc_filenames > all.md
+
+for file in all.md $doc_filenames
+do
+	name=$(basename $file .md)
+	md2html $file docs/$name.html $name
+	echo "- [$name](/$name.html)" >> index.md
+done
+
+md2html index.md docs/index.html "Bitcoin Core Docs"
